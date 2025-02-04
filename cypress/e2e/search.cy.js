8315b1445f105d84132dd95565113a53615897b3
Ewelina <ewewwis@gmail.com>
2025-02-04 13:56:10 +0100

    changes

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,7 +1,8 @@
 describe("Search", () => {
-    it("User should be able to search product", () => {
+    it("User should be able to search for a product", () => {
         cy.visit("/");
         cy.get('#search_query_top')
+        .should('be.visible')
         .click()
         .type('dress');
         cy.get('#search_query_top').should('have.value', 'dress');
